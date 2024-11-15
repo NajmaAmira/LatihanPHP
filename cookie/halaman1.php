e34f304af8f35a296d734d245148b3198e6d32cf
NajmaAmira <111202214708@mail.dinus.ac.id>
2024-11-15 17:28:34 +0700

    update final materi

---
+++ cookie/halaman1.php
@@ -0,0 +1,5 @@
+<?php
+// membuat cookie
+setcookie('nama', 'Najma', time()+60);
+
+?> 
---
+++ pertemuan20/tambah.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+
+if( !isset($_SESSION["login"]) ){
+    header("Location: login.php");
+    exit;
+}
+
+require 'functions.php';
+
+// cek apakah tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"])){
+
+    // cek apakah data berhasil ditambahkan atau tidak
+    if(tambah($_POST) > 0 ){
+        echo "
+            <script>
+                alert('Data berhasil ditambahkan!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    } else {
+        echo "
+            <script>
+                alert('Data gagal ditambahkan!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tambah Data Mahasiswa</title>
+    </head>
+    <body>
+        <h1>Tambah Data Mahasiswa</h1>
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <ul>
+                <li>
+                    <label for="nim">NIM : </label>
+                    <input type="text" name="nim" id="nim">
+                </li>
+                <li>
+                    <label for="nama">Nama : </label>
+                    <input type="text" name="nama" id="nama">
+                </li>
+                <li>
+                    <label for="email">Email : </label>
+                    <input type="text" name="email" id="email">
+                </li>
+                <li>
+                    <label for="prodi">Program Studi : </label>
+                    <input type="text" name="prodi" id="prodi">
+                </li>
+                <li>
+                    <label for="gambar">Gambar : </label>
+                    <input type="file" name="gambar" id="gambar">
+                </li>
+                <li>
+                    <button type="submit" name="submit">Tambah Data</button>
+                </li>
+            </ul>
+        </form>
+    </body>
+</html>
